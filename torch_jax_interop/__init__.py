754b64f2b4bb75b0d4574c2e816d98081df664cd
Fabrice Normandin <normandf@mila.quebec>
2024-06-06 09:34:09 -0400

    Export the new features in __init__.py
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- torch_jax_interop/__init__.py
+++ torch_jax_interop/__init__.py
@@ -1,5 +1,5 @@
 """A utility package for converting between PyTorch and JAX tensors."""
-from .to_torch import jax_to_torch
 from .to_jax import torch_to_jax
+from .to_torch import JaxFunction, JaxModule, jax_to_torch
 
-__all__ = ["jax_to_torch", "torch_to_jax"]
+__all__ = ["jax_to_torch", "torch_to_jax", "JaxModule", "JaxFunction"]
